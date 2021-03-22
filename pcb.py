db8ff7626a4609fb76fd84de318b2072379a45cb
Felieton <mateuszbroncel@gmail.com>
2021-03-22 17:59:26 +0100

    Delete old files

--- pcb.py
+++
@@ -1,5 +0,0 @@
-class PCB:
-    def __init__(self, width, height, list_of_connections):
-        self.width = width
-        self.height = height
-        self.list_of_connections = list_of_connections
--- point.py
+++
@@ -1,7 +0,0 @@
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __str__(self):
-        return f'({self.x}, {self.y})'
